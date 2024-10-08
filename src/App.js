93786ad9e02cb9fda04adacaa961acb005bc52bb
yvs-prasanna <yvskrishna159@gmail.com>
2024-10-09 01:45:25 +0530

    quizApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import EachQuestion from "./components/EachQuestion";
+import Header from "./components/Header";
+import Entername from "./components/Entername";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="Whole-Page">
+          <Header />
+          <Routes>
+            <Route exact path="/" Component={Entername} />
+            <Route path="/question" Component={EachQuestion} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
